6d8e51afcfcd6cfdc982b1a6123d22ab42edb841
mohsen <mohsen.1398.pg@gmail.com>
2020-07-05 04:46:51 -0700

    Create Utility_Context.js 1-create generic context for All Model

---
+++ api/Data_Layer/db/Context/Utility-Context.js
@@ -0,0 +1,25 @@
+let _db = require("../Models/index")
+let Generic_Repository = require("../Repository/Generice-Repository")
+let User = require("../Repository/User-Repository")
+class Utility_Context {
+    constructor() {
+        this.db = _db
+    }
+    User() {
+        if (!this.user) {
+            this.user = new User(this.db)
+            return this.user
+        }
+        return this.user
+    }
+
+    User_Password() {
+        if (!this.user_password) {
+            this.user_password = new Generic_Repository(this.db)
+            return this.user_password
+        }
+        return this.user_password
+    }
+
+}
+module.exports = new Utility_Context
