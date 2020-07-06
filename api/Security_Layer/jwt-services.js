66da665e427378fc32006bd3bc72b2a3d1a70154
mohsen <mohsen.1398.pg@gmail.com>
2020-07-06 03:10:18 -0700

    creat func CreateToken in jwt_services.js

--- api/Security_Layer/jwt-services.js
+++ api/Security_Layer/jwt-services.js
@@ -1,4 +1,7 @@
 class jwt_services {
-    
+     CreatToken(Data) {
+        Data.expLink = moment().add(30, "m").unix()
+        return jwt.sign(Data, process.env.Secret)
+    }
 }
 module.exports = new jwt_services
