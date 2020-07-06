3045e54c534c54d5d64b1f59e91ffa1f7729f8a9
mohsen <mohsen.1398.pg@gmail.com>
2020-07-06 04:02:01 -0700

    Update Services_User 1-writing the Login code for  user password

--- api/Data_Layer/User/Services_User.js
+++ api/Data_Layer/User/Services_User.js
@@ -1,19 +1,26 @@
 let Utility_Context = require("../db/Context/Utility-Context")
-let { jwt_service } = require("../../Security-Layer/index")
+let { argon_service } = require("../../Security-Layer/index")
 let { message } = require("../../View-Layer/index")
+const { user } = require("../db/Context/Utility-Context")
 class Services_User {
     async Login_User(Data) {
         await Utility_Context.Transaction(async () => {
             let User=await Utility_Context.User().Is_Exist_User(Data)
             if (User) {
-                User.User_Password = User.User_Passwords[0].get()
+                User.Password = User.User_Passwords[0].get()
                 delete User.User_Passwords
-               //return checkpassword
+                return is_currect_Password(User,Data)
             }
         }).catch(() => {
             message.SetMessage(message.Not_Verify_Email)
         })
         return message.GetMessage();
+    }
+    is_currect_Password(User,Data){
+        if (await argon_service().verifyhashing(Data.password, User.Password)) {
+            return User;
+          }
+          return null;
     }
 }
 module.exports = new Services_User
