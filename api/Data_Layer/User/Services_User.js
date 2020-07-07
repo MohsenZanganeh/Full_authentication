07367e9d8a38627791dce65050414400d89240a8
mohsen <mohsen.1398.pg@gmail.com>
2020-07-07 00:38:59 -0700

    create Register_User func 1-Email activation was set for email expiration 2-was Set 30m for Expires Email 3-was Set 10s for Expires resend 4-was set User_password in include option

--- api/Data_Layer/User/Services_User.js
+++ api/Data_Layer/User/Services_User.js
@@ -3,6 +3,27 @@ let { argon_service } = require("../../Security-Layer/index")
 let { message } = require("../../View-Layer/index")
 const { user } = require("../db/Context/Utility-Context")
 class Services_User {
+    async Register_User(Data) {
+        await Utility_Context.Transaction(async () => {
+            Data.User_Passwords = [{ password: await argon_service().hashingpassword(Data.password) }]
+            Data.activationEmailExpiresAt = moment().add(30, "m")
+            Data.activationEmailExpiresResend = moment().add(10, "s")
+            Data.activationCode = Generator.generateActivationCode();
+            //Property User 
+            let User = await Utility_Context.User().Insert(Data, {
+                include: [await Utility_Context.db.User_Password]
+            })
+            //Insert User
+            if (message.HaveError(User)) {
+                throw new Error();
+            }
+
+            message.SetMessage(User.JsonUser());
+        }).catch(() => {
+            message.SetMessage(message.Not_Verify_Email)
+        })
+        return message.GetMessage();
+    }
     async Login_User(Data) {
         await Utility_Context.Transaction(async () => {
             let User = await Utility_Context.User().Is_Exist_User(Data)
@@ -19,7 +40,8 @@ class Services_User {
         })
         return message.GetMessage();
     }
-    is_currect_Password(User, Data) {
+
+    async is_currect_Password(User, Data) {
         if (await argon_service().verifyhashing(Data.password, User.Password)) {
             return User;
         }
