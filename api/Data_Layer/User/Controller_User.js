e5412e61c98c7b10066f89fd117ab04067d1c550
mohsen <mohsen.1398.pg@gmail.com>
2020-07-07 01:00:36 -0700

    create Controller Register_User

--- api/Data_Layer/User/Controller_User.js
+++ api/Data_Layer/User/Controller_User.js
@@ -1,12 +1,19 @@
-let Service_User   = require ("./Services_User")
+let Service_User = require("./Services_User")
 let Validator_User = require("./Validator_User")
 class Controller_User {
-      async Login_User(req,res) {
-         let result =await Validator_User.Login_User(req,res)
-         if(result){
-           let User = await Service_User.Login_User(result)
-           res.send(User)
-         } 
-      }
+  async Register_User(req, res) {
+    let result = await Validator_User.Register_User(req, res)
+    if (result) {
+      let User = await Service_User.Register_User(result)
+      res.send(User)
+    }
+  }
+  async Login_User(req, res) {
+    let result = await Validator_User.Login_User(req, res)
+    if (result) {
+      let User = await Service_User.Login_User(result)
+      res.send(User)
+    }
+  }
 }
 module.exports = new Controller_User
