871eb1cb28baa90c77512ffa2c36d68dcd59adae
mohsen <mohsen.1398.pg@gmail.com>
2020-07-07 01:01:00 -0700

    create Router Register_User

--- api/APP/Router_User.js
+++ api/APP/Router_User.js
@@ -1,6 +1,6 @@
 let express=require("express")
 let Router=express();
 let Controller_User=require("../Data_Layer/User/Controller_User")
-
+Router.post("/register-user",Controller_User.Register_User)
 Router.get("/login-user",Controller_User.Login_User)
 module.exports=Router
