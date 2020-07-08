5e793870113184060d2c170f3a838fb09fadcb8e
mohsen <mohsen.1398.pg@gmail.com>
2020-07-08 13:53:59 -0700

    Create verify_Email Router

--- api/APP/Router_User.js
+++ api/APP/Router_User.js
@@ -3,4 +3,5 @@ let Router=express();
 let Controller_User=require("../Data_Layer/User/Controller_User")
 Router.post("/register-user",Controller_User.Register_User)
 Router.get("/login-user",Controller_User.Login_User)
+Router.get("/verify-email",Controller_User.Verifying_Email)
 module.exports=Router
