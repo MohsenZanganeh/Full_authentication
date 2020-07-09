3db2001b4e05f14a1a3b4de58c0131147cdeea6d
mohsen <mohsen.1398.pg@gmail.com>
2020-07-08 22:04:29 -0700

    Create Resend_Email Router

--- api/APP/Router_User.js
+++ api/APP/Router_User.js
@@ -4,4 +4,5 @@ let Controller_User=require("../Data_Layer/User/Controller_User")
 Router.post("/register-user",Controller_User.Register_User)
 Router.get("/login-user",Controller_User.Login_User)
 Router.get("/verify-email",Controller_User.Verifying_Email)
+Router.get("/resend-email",Controller_User.Resend_Email)
 module.exports=Router
