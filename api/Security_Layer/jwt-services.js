027b3b9794e14a2cdf3574c2b64defd292f051e3
mohsen <mohsen.1398.pg@gmail.com>
2020-07-08 23:09:11 -0700

    create Authenticat in jwt_Services.js

--- api/Security_Layer/jwt-services.js
+++ api/Security_Layer/jwt-services.js
@@ -1,28 +1,44 @@
-let moment=require("moment")
-let jwt=require("jsonwebtoken")
+let moment = require("moment")
+let jwt = require("jsonwebtoken")
 class jwt_services {
-     CreatToken(Data) {
-        Data.expLink = moment().add(30, "m").unix()
-        return jwt.sign(Data, process.env.Secret)
+  CreatToken(Data) {
+    Data.expLink = moment().add(30, "m").unix()
+    return jwt.sign(Data, process.env.Secret)
+  }
+  GetTokenFromUrl(req) {
+    return req.query.token != null ?
+      req.query.token :
+      req.header("Token");
+  }
+  CheckExpiresToken(decode) {
+    if (decode.expLink < moment().unix()) {
+      return false
     }
-    GetTokenFromUrl(req){
-        return  req.query.token!=null?
-          req.query.token:
-          req.header("Token") ;
-      }
-      CheckExpiresToken(decode){
-              if (decode.expLink < moment().unix()) {
-                 return false
-              }
-                return decode
-       }
-    VerifyToken(req) {        
-        let token = this.GetTokenFromUrl(req)
-        if (token) {
-            let decode = jwt.verify(token, process.env.Secret)
-            return this.CheckExpiresToken(decode)
+    return decode
+  }
+  VerifyToken(req) {
+    let token = this.GetTokenFromUrl(req)
+    if (token) {
+      let decode = jwt.verify(token, process.env.Secret)
+      return this.CheckExpiresToken(decode)
+    }
+    return false
+  }
+  async Authenticat(req, res, next) {
+    try {
+      let decode = this.VerifyToken(req)
+      if (!decode == false) {
+        let User = await Utility_Context.User().Is_Exist_User(decode.email)
+        if (!message.HaveError(User)) {
+          next()
+          return;
         }
-          return false
-     }
+        throw new Error()
+      }
+      throw new Error()
+    } catch (err) {
+      res.send(message.Not_Authenticat)
+    }
+  }
 }
 module.exports = new jwt_services
