ee6546fabe2056ee71e298089b5e4db5045ab0f2
mohsen <mohsen.1398.pg@gmail.com>
2020-07-08 22:00:32 -0700

    Create Resend_Email validator

--- api/Data_Layer/User/Validator_User.js
+++ api/Data_Layer/User/Validator_User.js
@@ -1,6 +1,6 @@
 let joi = require("joi")
 let { message } = require("../../View_Layer/index")
-let {jwt_service}=require("../../Security_Layer/index")
+let { jwt_service } = require("../../Security_Layer/index")
 class Validator_User {
     async Register_User(req, res) {
         const Register_schema = joi.object({
@@ -26,13 +26,24 @@ class Validator_User {
         return result
     }
     async Verifying_Email(req, res) {
-        let User=await jwt_service.VerifyToken(req)
+        let User = await jwt_service.VerifyToken(req)
         if (User) {
             return User
         } else {
             res.send(message.Activation_Code.Expires_Link);
         }
-}
+    }
+    async Resend_Email(req, res) {
+        let Resend_Email = joi.object({
+            email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+            uuid: joi.string()
+        })
+
+        let result = this.Validate(req, res, Resend_Email)
+        if (result) {
+          return result
+        }
+    }
     Validate(req, res, schema) {
         var _value;
         joi.validate(req.body, schema, (err, value) => {
