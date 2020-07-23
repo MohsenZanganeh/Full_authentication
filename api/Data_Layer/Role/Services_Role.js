e7d586641b8bfa10e6df3987f728b565bb2a2ee2
mohsen <mohsen.1398.pg@gmail.com>
2020-07-22 18:39:59 -0700

    Set Insert_Role code in service_Role this Is FOR TEST

--- api/Data_Layer/Role/Services_Role.js
+++ api/Data_Layer/Role/Services_Role.js
@@ -1,6 +1,21 @@
+let Utility_Context = require("../db/Context/Utility-Context")
 let {Generator}=require("../../Utility_Layer/index")
+const message = require("../../View_Layer/message")
 class Services_Role{
-
+    insert_Role(Data){
+        await Utility_Context.Transaction(async () => {
+            
+            Data.Permission = [Data.Permission]
+            //Property User 
+            let Role = await Utility_Context.Role().Insert(Data, {
+                include: [await Utility_Context.db.Permission]
+            })
+            message.SetMessage(Role);
+        }).catch(() => {
+            message.SetMessage()
+        })
+        return message.GetMessage();
+    }
 }
 
 module.exports=new Services_Role
