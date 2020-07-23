40af7a1ca2ed2fba1d46f85bce490d7b52c83964
mohsen <mohsen.1398.pg@gmail.com>
2020-07-22 18:56:51 -0700

    Set Insert_Role Code in Validator_Role

--- api/Data_Layer/Role/Validator_Role.js
+++ api/Data_Layer/Role/Validator_Role.js
@@ -1,7 +1,26 @@
 let joi = require("joi")
 let { message } = require("../../View_Layer/index")
 class Validator_Role {
- 
+    async Insert_Role(req, res) {
+        const objectSchema = joi.object({
+            name: joi.string().min(1).required(),
+            permission_code: joi.string().min(1).required()
+          }).required();
+          
+          const arraySchema = joi.array().items(objectSchema).min(1).unique()
+          .required();
+        const Insert_schema = joi.object({
+            responsibility: joi.string().empty(),
+            name: joi.string().empty(),
+            Permission: joi.alternatives().try(objectSchema, arraySchema).required()
+        })
+
+        let result = this.Validate(req, res, Insert_schema)
+
+        return result
+
+    }
+   
     Validate(req, res, schema) {
         var _value;
         joi.validate(req.body, schema, (err, value) => {
