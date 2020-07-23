2ba881e467dd947c7027c597b89786b83ab4f10f
mohsen <mohsen.1398.pg@gmail.com>
2020-07-22 19:02:01 -0700

    Create Controller_Role.js and Set First Code

---
+++ api/Data_Layer/Role/Controller_Role.js
@@ -0,0 +1,12 @@
+let Service_Role= require("./Services_Role")
+let Validator_Role = require("./Validator_Role")
+class Controller_Role {
+  async Insert_Role(req, res) {
+    let result = await Validator_Role(req, res)
+    if (result) {
+      let Role = await Service_Role.Insert_Role(result)
+      res.send(Role)
+    }
+  }
+}
+module.exports = new Controller_Role
