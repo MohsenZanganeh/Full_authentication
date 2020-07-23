6a5a9b49d2ac3b40be47778038f3641ca6158297
mohsen <mohsen.1398.pg@gmail.com>
2020-07-22 19:05:55 -0700

    Set Router Role in loader-Route

--- api/APP/loader-Route.js
+++ api/APP/loader-Route.js
@@ -2,7 +2,8 @@ let express=require("express")
 let Router=express();
 
 let User=require("./Router_User")
+let Role=require("./Router_Role")
 
 Router.use("/user",User)
-
+Router.use("/role",Role)
 module.exports=Router
